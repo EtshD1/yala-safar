3fe09eb88b756df6476b2e4d2c59d810bce187e1
Etsh <mohamed.h.abouelenin@gmail.com>
2022-01-20 15:55:47 +0200

    Header for the Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Navbar from "../Components/Navbar/Navbar";
 import styles from "../styles/Homepage.module.scss";
+import Icon from "../assets/icons/Search.svg";
+import Image from "next/image";
 
 const Home: NextPage = () => {
 	return (
@@ -15,6 +17,23 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Navbar />
+			<div className={styles.search}>
+				<div>
+					<h3>Search</h3>
+					<p>Search by location, property or address.</p>
+					<div className={styles.inputField}>
+						<label>
+							<div>
+								<Image src={Icon} />
+							</div>
+							<input
+								type="text"
+								placeholder="Search for Property"
+							/>
+						</label>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
