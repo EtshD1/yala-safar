71a487de4a18b84f0d8ebb60982398f5d968c3c7
Etsh <mohamed.h.abouelenin@gmail.com>
2022-02-02 12:51:09 +0200

    firebase config and html export script

---
+++ firebase.config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCm3NzVAOfg4aLq4PqkM9aj2-aarlamrxc",
+	authDomain: "yala-safar.firebaseapp.com",
+	projectId: "yala-safar",
+	storageBucket: "yala-safar.appspot.com",
+	messagingSenderId: "688546208346",
+	appId: "1:688546208346:web:74ea20f753196c80549663",
+};
+
+export default firebaseConfig;
